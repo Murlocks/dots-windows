fc9783dc159b37e78019afe6d1455e6ed647e288
Rene Tianco <mysterio2004173@gmail.com>
2024-08-07 02:06:37 -0400

    more mappings

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -9,9 +9,15 @@ map({ "n", "v" }, ";", ":", { desc = "Enter command mode" })
 map({ "i" }, "jk", "<esc>", { desc = "Enter normal mode" })
 
 -- Buffer Navigation (bufferline dependency)
-map("n", "<tab>", "<Cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" })
-map("n", "<s-tab>", "<Cmd>BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
+-- map("n", "<tab>", "<Cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" })
+-- map("n", "<s-tab>", "<Cmd>BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
 
 -- Line Navigation
-map("n", "H", "^", { desc = "Start of line" })
-map("n", "L", "$", { desc = "End of line" })
+-- map("n", "H", "^", { desc = "Start of line" })
+-- map("n", "L", "$", { desc = "End of line" })
+
+-- QOL
+map("n", "*", "*`'", { desc = "'*' but keep cursor place", silent = true })
+map("n", "#", "#`'", { desc = "'#' but keep cursor place", silent = true })
+map("n", "go", "m'o<esc>`'", { desc = "New line below cursor", silent = true })
+map("n", "gO", "m'O<esc>`'", { desc = "New line above cursor", silent = true })
